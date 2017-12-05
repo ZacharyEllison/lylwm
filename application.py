f0fe20188e066ad2336089d26fbf33892bd553f8
Zachary Ellison <zellison@college.harvard.edu>
2017-12-05 19:56:02 +0000

    Database-upload functioning, all working

--- application.py
+++ application.py
@@ -1,11 +1,12 @@
-from flask import Flask, flash, redirect, render_template, request, session, g
+from flask import Flask, flash, redirect, render_template
+from flask import request, session, send_from_directory, url_for, g
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions
 from werkzeug.security import check_password_hash, generate_password_hash
 from werkzeug.utils import secure_filename
 from pymongo import MongoClient
-from helpers import port, UPLOAD, usrnm, pswd
+from helpers import port, UPLOAD, usrnm, pswd, ALLOWED_EXTENSIONS, login_required
 import pymongo
 from datetime import datetime
 import os
@@ -25,6 +26,7 @@ now = datetime.now()
 
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config['DEBUG'] = True
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 app.config['UPLOAD_FOLDER'] = UPLOAD
@@ -38,49 +40,97 @@ def after_request(response):
     response.headers["Pragma"] = "no-cache"
     return response
 
+# have a check for allowed files (from Flask API)
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
 @app.route("/")
 def index():
     """Main Page"""
     return render_template("index.html")
 
 
-@app.route("/adventures/today")
+@app.route("/today")
+@login_required
 def today():
-    """This Page is meant to expand with time, for now it will be a way to look at what was uploaded or downloaded today by whom"""
+    """This Page is meant to expand with time,
+    for now it will be a way to look at what was
+    uploaded or downloaded today by whom"""
     flash("TODO")
     return redirect("/")
 
 
 
-@app.route("/adventures/explore.pg<pg_number>")
+@app.route("/adventures_pg?<path:pg_number>")
+@login_required
 def explore(pg_number):
     """Display files based on page"""
     flash("TODO")
     return redirect("/")
 
 
-@app.route("/adventures/download.<item_id>")
-def download(item_id):
+@app.route("/download?<path:fileid>")
+@login_required
+def download(filename):
     """Give download and redirect back to explore, the big thing here is the html"""
     flash("TODO")
-    return redirect("/")
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+
+@app.route("/delete?<path:fileid>")
+@login_required
+def delete(filename):
+    flash("TODO")
+    return redirect('/')
 
 
 @app.route("/upload", methods=["GET", "POST"])
+@login_required
 def upload():
     """Uploads a file to the db and home folder"""
+    tags=[]
 
-    tags = items.distinct( "tags" )
+    if items.distinct( "tags" ):
+        tags = items.distinct( "tags" )
 
     # Flask documentation shows uploads are the following
     if request.method == 'POST':
+        # check if the post request has the file
+        if 'file' not in request.files:
+            flash('No file')
+            return redirect(request.url)
+
         file = request.files['file']
-        # I will add to the database here
-        # I want to track the filename, size, type, date added, user who added, tags, permissions,
-        file.save(os.path.join(UPLOAD, secure_filename(file.filename)))
-        """TODO"""
-        flash("File uploaded successfully")
-        return redirect("/")
+
+        # if user does not select file
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+
+        if file and allowed_file(file.filename):
+
+            file.save(os.path.join(UPLOAD, secure_filename(file.filename)))
+
+            # I will add to the database here
+            # I want to track the filename, size, type, date added, user who added, tags, permissions,
+            items.insert_one({
+                "name": file.filename,
+                "type": file.filename.split(".")[1],
+                "size": os.stat(os.path.join(UPLOAD, secure_filename(file.filename))).st_size,
+                "location": os.path.join(UPLOAD, secure_filename(file.filename)),
+                "owner": session["user_id"],
+                "permission": request.form.get("permission"),
+                "tags": request.form.get("new_tag").split(" "),
+                "date": datetime.now()
+            })
+
+            flash("File uploaded successfully")
+            return redirect("/")
+        else:
+            flash("Unsupported file type")
+            return redirect(url_for("upload"))
     else:
         return render_template("upload.html", tags=tags)
 
@@ -104,8 +154,8 @@ def register():
         # If the username exists in the db
         if users.find_one({usrnm: usr}):
             # Attempt to limit the steps the user takes with a flash and redirect back, so no error page
-            return render_template("register.html", usrtkn=True)
-
+            flash("username taken")
+            return render_template("register.html")
         # Make an object that represents the new user to be inserted
         new_user = {'username': usr, 'password': generate_password_hash(pswd_get)}
 
@@ -116,7 +166,7 @@ def register():
         flash("Account successfully created")
         # Remember which user has logged in
         session["user_id"] = str(new_id)
-        return redirect("/")
+        return redirect(url_for('index'))
 
     # User reached route via GET (as by clicking a link or via redirect)
     else:
@@ -132,7 +182,7 @@ def logout():
 
     # Redirect user to login form
     flash("Logout successful")
-    return redirect("/")
+    return redirect(url_for('index'))
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -167,7 +217,7 @@ def login():
 
         # Redirect user to home page
         flash("Successfully logged in")
-        return redirect("/")
+        return redirect(url_for('index'))
 
     # User reached route via GET (as by clicking a link or via redirect)
     else:
--- helpers.py
+++ helpers.py
@@ -1,13 +1,32 @@
+import csv
+import urllib.request
+
+from flask import redirect, render_template, request, session
+from functools import wraps
+
 """This should be filled with magic numbers and helper functions"""
+
 
 port = 31416
 UPLOAD='static/home/'
 usrnm = 'username'
 pswd = 'password'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'doc', 'docx'])
 
-class Document:
+def login_required(f):
+    """
+    Decorate routes to require login.
 
-    """Here there should be
+    http://flask.pocoo.org/docs/0.12/patterns/viewdecorators/
+    """
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function
+
+"""Here there should be
         string filename
         string filetype
         string file size
@@ -15,4 +34,4 @@ class Document:
         string user who uploaded
         whether others can delete
         But might not need this
-    """
+"""
